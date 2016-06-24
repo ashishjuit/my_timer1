6c7286517e58ce0de8e3c8b881b4ae56a4128051
ASHISH AGGARWAL <ashishjyc@gmail.com>
2016-06-24 13:23:09 -0400

    old timer script added

---
+++ my_timer1/public/js/app.js
@@ -0,0 +1,104 @@
+$(document).ready(function(){
+  var start = $('#start');
+  var minutes = $('#minutes');
+  var seconds = $('#seconds');
+  var pauseBtn = $('#pauseBtn');
+  var breakBtn = $('#breakBtn');
+  var countdown;
+  var countdown1;
+  var snd = new Audio("http://www.websites-graphics1.de/songs/wavs/1wav_geraeuche/click_one.wav");
+  var audio = new Audio('http://www.soundjay.com/switch/sounds/switch-4.mp3');
+
+
+  start.on('click', startReverseCountdown);
+  pauseBtn.on('click', pauseOn);
+  breakBtn.on('click', breakOn);
+  // breakBtn.on('click', startCountdown);
+
+  function pauseOn(){
+    snd.play();
+    clearTimeout(countdown);
+    clearTimeout(countdown1);
+  }
+
+
+  function breakOn(){
+    minutes.text('05');
+    seconds.text('00');
+    startCountdown();
+
+
+  }
+
+  function startReverseCountdown(){
+           snd.play();
+          countdown = setInterval(function(){
+            audio.play();
+      var secondsVal=+seconds.text();
+      var minutesVal=+minutes.text();
+
+      if(minutesVal>=25){
+        breakBtn.removeClass('disabled');
+        breakBtn.removeAttr('disabled');
+      }
+      if(secondsVal<59){
+        if(secondsVal<9){
+          seconds.text("0"+(secondsVal+1))
+        }
+        else{
+          seconds.text(secondsVal+1);
+        }
+      }
+        else if (secondsVal>58) {
+        seconds.text("00");
+      }
+
+        if(secondsVal===59){
+          if(minutesVal<9){
+            minutes.text('0' + (minutesVal+1));
+          }
+          else{
+            minutes.text(minutesVal+1);
+          }
+        }
+    },1000);
+  }
+
+
+  function startCountdown(){
+           snd.play();
+      clearInterval(countdown);
+      // breakBtn.addClass('disabled');
+      // breakBtn.addAttr('disabled');
+            countdown1 = setInterval(function(){
+        audio.play();
+      var secondsVal1=+seconds.text();
+      var minutesVal1=+minutes.text();
+      if(secondsVal1===0 && minutesVal1===0){
+        return;
+      }
+
+      if(secondsVal1===0){
+        seconds.text(59);
+      }else {
+        if(secondsVal1<11){
+          seconds.text("0" + (secondsVal1 -1))
+        }
+        else{
+          seconds.text(secondsVal1 - 1);
+        }
+      }
+
+      if(secondsVal1===0){
+        if(minutesVal1<11){
+          minutes.text("0" + (minutesVal1 - 1))
+        }
+        else{
+          minutes.text(minutesVal1-1);
+        }
+      }
+
+    }, 1000);
+  }
+
+});
